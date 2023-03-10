81c51de43b210c1c3589aa72da64c258e07b693a
Hasani Rogers <hasani.rogers@gmail.com>
2023-03-10 17:27:33 -0500

    converted badge to ts

---
+++ src/components/kemet-badge/kemet-badge.ts
@@ -0,0 +1,53 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { stylesBase } from './styles';
+
+/**
+ * @since 1.0.0
+ * @status stable
+ *
+ * @tagname kemet-badge
+ * @summary Badges display the status of information.
+ *
+ * @prop {string} status - The status of the badge
+ * @prop {boolean} circle - Displays the badge in a circle
+ * @prop {boolean} pill - Rounds the corners on the badge
+ * @prop {number} circlePadding - Padding on the badge as a circle
+ *
+ * @cssproperty --kemet-badge-padding - The padding of the badge. Default: 10px.
+ *
+ */
+
+@customElement('kemet-badge')
+export default class KemetBadge extends LitElement {
+  static styles = [stylesBase];
+
+  @property({ type: String, reflect: true})
+  status: string = 'primary';
+
+  @property({ type: Boolean, reflect: true })
+  circle: boolean = false;
+
+  @property({ type: Boolean, reflect: true })
+  pill: boolean = false;
+
+  @property({ type: Number, attribute: 'circle-padding' })
+  circlePadding: number = 0;
+
+  render() {
+    return html`<slot @slotchange=${() => this.handleSlotChange()}></slot>`;
+  }
+
+  handleSlotChange() {
+    if (this.circle) {
+      this.style.height = `${this.offsetWidth + this.circlePadding}px`;
+      this.style.width = `${this.offsetWidth + this.circlePadding}px`;
+    }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'kemet-badge': KemetBadge
+  }
+}
