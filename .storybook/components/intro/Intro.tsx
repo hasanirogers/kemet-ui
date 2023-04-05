270d3f652496ee69fba73198f5eacd4fb3e535c9
Hasani Rogers <hasani.rogers@gmail.com>
2023-04-05 10:31:35 -0400

    added htmlbox + js build upgrade

---
+++ .storybook/components/intro/Intro.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './Intro.css';
+
+export const Intro = () => {
+  return (
+    <div className="storybook__intro">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+          <path d="M20 14c-.092.064-2 2.083-2 3.5 0 1.494.949 2.448 2 2.5.906.044 2-.891 2-2.5 0-1.5-1.908-3.436-2-3.5zM9.586 20c.378.378.88.586 1.414.586s1.036-.208 1.414-.586l7-7-.707-.707L11 4.586 8.707 2.293 7.293 3.707 9.586 6 4 11.586c-.378.378-.586.88-.586 1.414s.208 1.036.586 1.414L9.586 20zM11 7.414 16.586 13H5.414L11 7.414z"/>
+        </svg>
+        <h3>Styles</h3>
+        <p>Kemet UI ships with Base, Core API, and Component Styles. The Base Styles are required and include custom properties for styling. The Core API styles are Kemet UI's custom attributes for layout and building a design system. See the Config and Core API pages for more information. The Component Styles are optional and simply enhance the look of some components. Most components styles are baked in the component.</p>
+      </div>
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" height="20" viewBox="0 0 20 20" width="20"><path d="M3 4C3 3.44772 3.44772 3 4 3H16C16.5523 3 17 3.44772 17 4V6C17 6.55228 16.5523 7 16 7H4C3.44772 7 3 6.55228 3 6V4Z" /><path d="M3 10C3 9.44771 3.44772 9 4 9H10C10.5523 9 11 9.44771 11 10V16C11 16.5523 10.5523 17 10 17H4C3.44772 17 3 16.5523 3 16V10Z" /><path d="M14 9C13.4477 9 13 9.44771 13 10V16C13 16.5523 13.4477 17 14 17H16C16.5523 17 17 16.5523 17 16V10C17 9.44771 16.5523 9 16 9H14Z" /></svg>
+        <h3>Templates</h3>
+        <p>You'll find various pre-cooked templates using Kemet UI here. These templates are largely inspired by popular frameworks such as Bootstrap and Foundation. While you can grab the code for the templates here in the docs, <a href="https://stackblitz.com/edit/kemet-templates?file=index.html">if you want to play with the templates be sure to checkout this Stackblitz</a>.</p>
+      </div>
+      <div>
+        <svg viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path d="M512.1 191l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0L552 6.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zm-10.5-58.8c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.7-82.4 14.3-52.8 52.8zM386.3 286.1l33.7 16.8c10.1 5.8 14.5 18.1 10.5 29.1-8.9 24.2-26.4 46.4-42.6 65.8-7.4 8.9-20.2 11.1-30.3 5.3l-29.1-16.8c-16 13.7-34.6 24.6-54.9 31.7v33.6c0 11.6-8.3 21.6-19.7 23.6-24.6 4.2-50.4 4.4-75.9 0-11.5-2-20-11.9-20-23.6V418c-20.3-7.2-38.9-18-54.9-31.7L74 403c-10 5.8-22.9 3.6-30.3-5.3-16.2-19.4-33.3-41.6-42.2-65.7-4-10.9.4-23.2 10.5-29.1l33.3-16.8c-3.9-20.9-3.9-42.4 0-63.4L12 205.8c-10.1-5.8-14.6-18.1-10.5-29 8.9-24.2 26-46.4 42.2-65.8 7.4-8.9 20.2-11.1 30.3-5.3l29.1 16.8c16-13.7 34.6-24.6 54.9-31.7V57.1c0-11.5 8.2-21.5 19.6-23.5 24.6-4.2 50.5-4.4 76-.1 11.5 2 20 11.9 20 23.6v33.6c20.3 7.2 38.9 18 54.9 31.7l29.1-16.8c10-5.8 22.9-3.6 30.3 5.3 16.2 19.4 33.2 41.6 42.1 65.8 4 10.9.1 23.2-10 29.1l-33.7 16.8c3.9 21 3.9 42.5 0 63.5zm-117.6 21.1c59.2-77-28.7-164.9-105.7-105.7-59.2 77 28.7 164.9 105.7 105.7zm243.4 182.7l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0l8.2-14.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zM501.6 431c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.6-82.4 14.3-52.8 52.8z"/></svg>
+        <h3>Components</h3>
+        <p>Kemet UI has a number of components which embed behavior for your UI. These components are highly flexible and each Story is written in a way that allows you to see how you can customize the look and feel of the component to suite your designs.</p>
+      </div>
+    </div>
+  );
+};
--- config/rollup.mjs
+++ config/rollup.mjs
@@ -1,28 +1,32 @@
 import resolve from '@rollup/plugin-node-resolve';
 import multi from '@rollup/plugin-multi-entry';
-import copy from 'rollup-plugin-copy';
-// import minifyHTML from 'rollup-plugin-minify-html-literals';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
+import pkgMinifyHTML from 'rollup-plugin-minify-html-literals';
 
-export default {
-  input: 'src/components/kemet-*/kemet-*.js',
-  output: [
-    { file: 'dist/kemet-components.js', format: 'esm' },
-    { file: 'dist/kemet-components.cjs', format: 'cjs' },
-    { file: 'dist/kemet-components.min.js', format: 'esm', plugins: [terser()] },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    multi(),
-    copy({
-      targets: [
-        { src: ['src/components/kemet-*/!(demo)', '!**/stories.*', '!**/test.js'], dest: 'dist' },
-        { src: 'src/utilities', dest: 'dist' },
-        { src: 'src/styles/!(demo)', dest: 'dist' },
-      ],
-      flatten: false,
-    }),
-  ],
-};
+const minifyHTML = pkgMinifyHTML.default;
+
+export default [
+  // for bundlers
+  {
+    input: ['dist/components/kemet-*/kemet-*.js'],
+    output: [
+      { file: 'dist/kemet-components.js', format: 'esm' },
+      { file: 'dist/kemet-components.cjs', format: 'cjs' },
+    ],
+    plugins: [resolve(), commonjs(), multi()],
+  },
+  // for browsers
+  {
+    input: ['dist/components/kemet-*/kemet-*.js'],
+    output: [
+      {
+        file: 'dist/kemet-components.bundle.js',
+        name: 'kemet-components',
+        format: 'umd',
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), multi(), minifyHTML(), terser()],
+  },
+];
