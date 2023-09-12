6facccc391da22fe09acb55288be0740b602f995
Hasani Rogers <hasani.rogers@gmail.com>
2023-09-12 10:50:52 -0400

    correct colors for combo field and input

--- src/components/kemet-combo/styles.ts
+++ src/components/kemet-combo/styles.ts
@@ -2,9 +2,19 @@ import { css } from 'lit';
 
 export const stylesBase = css`
   :host {
+    --kemet-combo-width: calc(100% - 2px);
+    --kemet-combo-margin: 1rem auto;
+    --kemet-combo-max-height: calc(5 * 3rem);
+    --kemet-combo-border: 1px solid rgb(var(--kemet-color-foreground));
+    --kemet-combo-border-radius: var(--kemet-border-radius);
+    --kemet-combo-background-color: rgb(var(--kemet-color-white-to-black));
+    --kemet-combo-shadow: var(--kemet-elevation-layer5);
+    --kemet-combo-hover-color: rgb(var(--kemet-color-white));
+    --kemet-combo-hover-background-color: rgb(var(--kemet-color-primary));
+
     opacity: 0;
-    width: var(--kemet-combo-width, calc(100% - 2px));
-    margin: var(--kemet-combo-margin, 1rem auto);
+    width: var(--kemet-combo-width);
+    margin: var(--kemet-combo-margin);
     pointer-events: none;
     display: block;
     position: relative;
@@ -18,27 +28,29 @@ export const stylesBase = css`
 
   ul {
     width: 100%;
-    max-height: var(--kemet-combo-max-height, calc(5 * 3rem));
+    max-height: var(--kemet-combo-max-height);
     position: absolute;
     z-index: 1;
     list-style: none;
     margin: 0;
     padding: 0;
     overflow-y: scroll;
-    border: var(--kemet-combo-border, 1px solid var(--kemet-color-background));
-    border-radius: var(--kemet-combo-border-radius, var(--kemet-border-radius));
-    background-color: var(--kemet-combo-background-color, var(--kemet-color-white-to-black));
-    box-shadow: var(--kemet-combo-shadow, var(--kemet-elevation-layer5));
+    border: var(--kemet-combo-border);
+    border-radius: var(--kemet-combo-border-radius);
+    background-color: var(--kemet-combo-background-color);
+    box-shadow: var(--kemet-combo-shadow);
   }
+
   li {
     line-height: 3rem;
     padding: 0 1.5rem;
     cursor: pointer;
   }
+
   li:hover,
   li:focus {
-    color: var(--kemet-combo-hover-color, var(--kemet-color-white));
+    color: var(--kemet-combo-hover-color);
     outline: none;
-    background-color: var(--kemet-combo-hover-background-color, var(--kemet-color-primary));
+    background-color: var(--kemet-combo-hover-background-color);
   }
 `;
--- src/components/kemet-input/styles.ts
+++ src/components/kemet-input/styles.ts
@@ -2,18 +2,35 @@ import { css } from 'lit';
 
 export const stylesBase = css`
   :host {
+    --kemet-input-height: 50px;
+    --kemet-input-padding: 0.5rem 1rem;
+    --kemet-input-border: 1px solid rgb(var(--kemet-color-foreground));
+    --kemet-input-border-color-error: 1px solid rgb(var(--kemet-color-error));
+    --kemet-input-border-color-success: 1px solid rgb(var(--kemet-color-success));
+    --kemet-input-border-color-warning: 1px solid rgb(var(--kemet-color-warning));
+    --kemet-input-icon-left-padding: 3rem;
+    --kemet-input-icon-right-padding: 3rem;
+    --kemet-input-border-radius-rounded: var(--kemet-border-radius);
+    --kemet-input-border-filled: none;
+    --kemet-input-color-filled: rgb(var(--kemet-color-white));
+    --kemet-input-background-color-filled: rgb(var(--kemet-color-primary));
+    --kemet-input-color-filled: rgb(var(--kemet-color-white));
+    --kemet-input-background-color-error: rgb(var(--kemet-color-error));
+    --kemet-input-background-color-success: rgb(var(--kemet-color-success));
+    --kemet-input-background-color-warning: rgb(var(--kemet-color-warning));
+
     position: relative;
     display: block;
   }
 
   input {
-    color: var(--kemet-color-text);
+    color: rgb(var(--kemet-color-text));
     display: block;
     font-size: 1rem;
     width: 100%;
-    height: var(--kemet-input-height, 50px);
-    padding: var(--kemet-input-padding, 0.5rem 1rem);
-    border: var(--kemet-input-border, 1px solid var(--kemet-color-background));
+    height: var(--kemet-input-height);
+    padding: var(--kemet-input-padding);
+    border: var(--kemet-input-border);
     appearance: none;
     box-sizing: border-box;
     background-color: transparent;
@@ -31,15 +48,15 @@ export const stylesBase = css`
   }
 
   :host([status='error']) input {
-    border: var(--kemet-input-border-color-error, 1px solid var(--kemet-color-error));
+    border: var(--kemet-input-border-color-error);
   }
 
   :host([status='success']) input {
-    border: var(--kemet-input-border-color-success, 1px solid var(--kemet-color-success));
+    border: var(--kemet-input-border-color-success);
   }
 
   :host([status='warning']) input {
-    border: var(--kemet-input-border-color-warning, 1px solid var(--kemet-color-warning));
+    border: var(--kemet-input-border-color-warning);
   }
 
   :host([disabled]) input {
@@ -47,42 +64,42 @@ export const stylesBase = css`
   }
 
   :host([icon-left]) input {
-    padding-left: var(--kemet-input-icon-left-padding, 3rem);
+    padding-left: var(--kemet-input-icon-left-padding);
   }
 
   :host([icon-right]) input {
-    padding-right: var(--kemet-input-icon-right-padding, 3rem);
+    padding-right: var(--kemet-input-icon-right-padding);
   }
 
   :host([rounded]) input {
-    border-radius: var(--kemet-input-border-radius-rounded, var(--kemet-border-radius));
+    border-radius: var(--kemet-input-border-radius-rounded);
   }
 
   :host([filled]) input {
-    border: var(--kemet-input-border-filled, none);
-    color: var(--kemet-input-color-filled, var(--kemet-color-white));
-    background-color: var(--kemet-input-background-color-filled, var(--kemet-color-primary));
+    border: var(--kemet-input-border-filled);
+    color: var(--kemet-input-color-filled);
+    background-color: var(--kemet-input-background-color-filled);
   }
 
   :host([filled]) kemet-icon,
   :host([filled]) input::placeholder {
-    color: var(--kemet-input-color-filled, var(--kemet-color-white));
+    color: var(--kemet-input-color-filled);
   }
 
   :host([status='error'][filled]) input {
-    background-color: var(--kemet-input-background-color-error, var(--kemet-color-error));
+    background-color: var(--kemet-input-background-color-error);
   }
 
   :host([status='success'][filled]) input {
-    background-color: var(--kemet-input-background-color-success, var(--kemet-color-success));
+    background-color: var(--kemet-input-background-color-success);
   }
 
   :host([status='warning'][filled]) input {
-    background-color: var(--kemet-input-background-color-warning, var(--kemet-color-warning));
+    background-color: var(--kemet-input-background-color-warning);
   }
 
   kemet-icon {
-    color: var(--kemet-color-text);
+    color: rgb(var(--kemet-color-text));
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
@@ -102,14 +119,14 @@ export const stylesBase = css`
   }
 
   :host([status='error']) kemet-icon {
-    color: var(--kemet-color-error);
+    color: rgb(var(--kemet-color-error));
   }
 
   :host([status='warning']) kemet-icon {
-    color: var(--kemet-color-error);
+    color: rgb(var(--kemet-color-error));
   }
 
   :host([status='success']) kemet-icon {
-    color: var(--kemet-color-success);
+    color: rgb(var(--kemet-color-success));
   }
 `;
