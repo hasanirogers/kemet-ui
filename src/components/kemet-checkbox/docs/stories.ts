cc2c44aef101b065b9e0f43c225a99af44c5cff7
Hasani Rogers <hasani.rogers@gmail.com>
2023-10-01 21:32:03 -0400

    updated checkbox docs

--- src/components/kemet-checkbox/docs/stories.ts
+++ src/components/kemet-checkbox/docs/stories.ts
@@ -10,50 +10,47 @@ import '../../kemet-tabs/kemet-tab-panel';
 const meta: Meta = {
   title: 'Components / Checkbox',
   component: 'kemet-checkbox',
+  args: {
+    label: 'Label',
+  },
 };
 export default meta;
 
 type Story = StoryObj;
 
-const Template = ({
-  label = 'Label',
-  checked = false,
-  indeterminate = false,
-  disabled = false,
-  rounded = false,
-  filled = false,
-}) => html`
-  <kemet-checkbox label=${label} ?checked=${checked} ?indeterminate=${indeterminate} ?disabled=${disabled} ?rounded=${rounded} ?filled=${filled}></kemet-checkbox>
+const Template = (args) => html`
+  <kemet-checkbox label=${args.label} ?checked=${args.checked} ?indeterminate=${args.indeterminate} ?disabled=${args.disabled} ?rounded=${args.rounded} ?filled=${args.filled}></kemet-checkbox>
 `;
 
 export const Standard: Story = {
   render: args => Template(args),
+};
+
+export const Indeterminate: Story = {
+  render: args => Template(args),
   args: {
-    label: 'Label',
-    checked: false,
-    indeterminate: false,
-    disabled: false,
-    rounded: false,
-    filled: false,
-  },
-  argTypes: {
-    label: {
-      control: 'text',
-    },
-    checked: {
-      control: 'boolean',
-    },
-    indeterminate: {
-      control: 'boolean',
-    },
-    disabled: {
-      control: 'boolean',
-    },
-    rounded: {
-      control: 'boolean',
-    },
-    filled: {
-      control: 'boolean',
-    },
+    indeterminate: true,
+  }
+};
+
+export const Disabled: Story = {
+  render: args => Template(args),
+  args: {
+    disabled: true,
+  }
+};
+
+export const Rounded: Story = {
+  render: args => Template(args),
+  args: {
+    rounded: true,
+  }
+};
+
+export const Filled: Story = {
+  render: args => Template(args),
+  args: {
+    checked: true,
+    filled: true,
   }
 };
