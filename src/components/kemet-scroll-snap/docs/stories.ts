9681189cacd7919c42568e77874038d872927e75
Hasani Rogers <hasani.rogers@gmail.com>
2023-10-28 19:41:08 -0400

    updated docs

--- src/components/kemet-scroll-snap/docs/stories.ts
+++ src/components/kemet-scroll-snap/docs/stories.ts
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import type { Meta, StoryObj } from '@storybook/web-components';
 
 import '../kemet-scroll-snap';
@@ -13,36 +14,35 @@ import '../../kemet-icon/kemet-icon';
 const meta: Meta = {
   title: 'Components / Scroll Snap',
   component: 'kemet-scroll-snap',
+  args: {
+    numberOfSlides: 5,
+  },
+  argTypes: {
+    axis: {
+      control: { type: 'select' },
+      options: ['horizontal', 'vertical'],
+    },
+    pagination: {
+      control: { type: 'select' },
+      options: ['top', 'right', 'bottom', 'left'],
+    },
+  }
 };
 export default meta;
 
 type Story = StoryObj;
 
-const Template = ({
-  showPagination = true,
-  numberOfSlides = 5,
-  gap = '1.5rem',
-  axis = 'horizontal',
-  pagination = 'bottom',
-  paginationIcon = 'circle-fill',
-  paginationSize = 16,
-  paginationUseNumbers = false,
-  paginationUseLabels = false,
-  paginationCenter = true,
-  paginationGap = '0.5rem',
-  slideWidth = '44vw',
-  horizontalMaxWidth = '90%',
-}) => {
+const Template = (args) => {
   const makeSlides = (numberOfSlides) => {
     const slides = [];
 
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < numberOfSlides; i++) {
       slides.push(html`
-        <kemet-scroll-snap-slide label="Heading ${i + 1}" kemet-margin-top="2xl">
+        <kemet-scroll-snap-slide label="Heading ${i + 1}" kemet-margin-top="2xl" style=${ifDefined(args.slideWidth ? `--kemet-scroll-snap-slide-width: ${args.slideWidth};` : undefined)}>
           <h3 kemet-type-size="3xl">Heading ${i + 1}</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <img src="https://via.placeholder.com/1920x1080" alt="A placeholder" />
+          <img src="https://via.placeholder.com/1920x1080" alt="A placeholder" style="max-width:100%" />
         </kemet-scroll-snap-slide>
       `);
     }
@@ -55,8 +55,8 @@ const Template = ({
       return html`
         <kemet-scroll-snap-paginator
           slot="pagination"
-          icon="${icon}"
-          size="${size}"
+          icon="${ifDefined(icon)}"
+          size="${ifDefined(size)}"
           ?use-number-pages=${useNumbers}
           ?use-label-pages=${useLabels}
           ?center=${center}>
@@ -68,91 +68,73 @@ const Template = ({
   };
 
   return html`
-    <style>
-      img {
-        max-width: 100%;
-      }
-
-      kemet-scroll-snap {
-        --kemet-scroll-snap-gap: ${gap};
-        --kemet-scroll-snap-horizontal-max-width: ${horizontalMaxWidth};
-      }
-
-      kemet-scroll-snap-slide {
-        --kemet-scroll-snap-slide-width: ${slideWidth};
-      }
-
-      kemet-scroll-snap-paginator {
-        --kemet-scroll-snap-paginator-gap: ${paginationGap};
-      }
-    </style>
-    <kemet-scroll-snap axis="${axis}" pagination="${pagination}">
+    <kemet-scroll-snap axis="${ifDefined(args.axis)}" pagination="${ifDefined(args.pagination)}">
       <div slot="slides">
-        ${makeSlides(numberOfSlides)}
+        ${makeSlides(args.numberOfSlides)}
       </div>
-      ${makePagination(showPagination, paginationIcon, paginationSize, paginationUseNumbers, paginationUseLabels, paginationCenter)}
+      ${makePagination(args.showPagination, args.paginationIcon, args.paginationSize, args.paginationUseNumbers, args.paginationUseLabels, args.paginationCenter)}
     </kemet-scroll-snap>
   `;
 }
 
+
+// export const Standard: Story = {
+//   render: args => Template(args),
+//   args: {
+//     showPagination: true,
+//     numberOfSlides: 5,
+//     gap: '1.5rem',
+//     axis: 'horizontal',
+//     pagination: 'bottom',
+//     paginationIcon: 'circle-fill',
+//     paginationSize: 16,
+//     paginationUseNumbers: false,
+//     paginationUseLabels: false,
+//     paginationCenter: true,
+//     paginationGap: '0.5rem',
+//     slideWidth: '44vw',
+//     horizontalMaxWidth: '90%',
+//   },
+// };
+
 export const Standard: Story = {
+  render: args => Template(args),
+};
+
+export const Vertical: Story = {
+  render: args => Template(args),
+  args: {
+    axis: 'vertical',
+  },
+};
+
+export const Pagination: Story = {
   render: args => Template(args),
   args: {
     showPagination: true,
-    numberOfSlides: 5,
-    gap: '1.5rem',
-    axis: 'horizontal',
-    pagination: 'bottom',
-    paginationIcon: 'circle-fill',
-    paginationSize: 16,
-    paginationUseNumbers: false,
-    paginationUseLabels: false,
-    paginationCenter: true,
-    paginationGap: '0.5rem',
-    slideWidth: '44vw',
-    horizontalMaxWidth: '90%',
   },
-  argTypes: {
-    showPagination: {
-      control: { type: 'boolean' },
-    },
-    numberOfSlides: {
-      control: { type: 'number' },
-    },
-    gap: {
-      control: { type: 'text' },
-    },
-    axis: {
-      control: { type: 'select' },
-      options: ['horizontal', 'vertical'],
-    },
-    pagination: {
-      control: { type: 'select' },
-      options: ['top', 'right', 'bottom', 'left'],
-    },
-    paginationIcon: {
-      control: { type: 'text' },
-    },
-    paginationSize: {
-      control: { type: 'number' },
-    },
-    paginationUseNumbers: {
-      control: { type: 'boolean' },
-    },
-    paginationUseLabels: {
-      control: { type: 'boolean' },
-    },
-    paginationCenter: {
-      control: { type: 'boolean' },
-    },
-    paginationGap: {
-      control: { type: 'text' },
-    },
-    slideWidth: {
-      control: { type: 'text' },
-    },
-    horizontalMaxWidth: {
-      control: { type: 'text' },
-    },
-  }
 };
+
+export const PaginationLabels: Story = {
+  render: args => Template(args),
+  args: {
+    showPagination: true,
+    paginationUseLabels: true,
+  },
+};
+
+export const PaginationNumbers: Story = {
+  render: args => Template(args),
+  args: {
+    showPagination: true,
+    paginationUseNumbers: true,
+  },
+};
+
+export const MultipleSlides: Story = {
+  render: args => Template(args),
+  args: {
+    showPagination: true,
+    slideWidth: '33vw',
+  },
+}
