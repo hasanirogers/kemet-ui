24a6323d97c5808ce166f4d720496755c66976a0
Hasani Rogers <hasani.rogers@gmail.com>
2023-10-29 14:40:55 -0400

    updated docs

--- src/components/kemet-tracker/docs/stories.ts
+++ src/components/kemet-tracker/docs/stories.ts
@@ -1,4 +1,5 @@
 import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import type { Meta, StoryObj } from '@storybook/web-components';
 
 import '../kemet-tracker';
@@ -11,25 +12,29 @@ import '../../kemet-tabs/kemet-tab-panel';
 const meta: Meta = {
   title: 'Components / Tracker',
   component: 'kemet-tracker',
+  args: {
+    numOfSteps: 5,
+    currentStep: 3,
+  },
+  argTypes: {
+    currentStep: {
+      control: { type: 'number', max: 5 },
+    },
+  }
 };
 export default meta;
 
 type Story = StoryObj;
 
-const Template = ({
-  breakpoint = '767px',
-  numOfSteps = 5,
-  currentStep = 3,
-  hideDotContent = false,
-}) => {
+const Template = (args) => {
   const steps = [];
 
-  for (let i = 1; i <= numOfSteps; i += 1) {
-    steps.push(html`<kemet-tracker-step ?completed=${i < currentStep || currentStep > numOfSteps} ?current=${i === currentStep} ?hide-dot-content=${hideDotContent}>Step ${i}</kemet-tracker-step>`);
+  for (let i = 1; i <= args.numOfSteps; i += 1) {
+    steps.push(html`<kemet-tracker-step ?completed=${i < args.currentStep || args.currentStep > args.numOfSteps} ?current=${i === args.currentStep} ?hide-dot-content=${args.hideDotContent}>Step ${i}</kemet-tracker-step>`);
   }
 
   return html`
-    <kemet-tracker breakpoint=${breakpoint}>
+    <kemet-tracker breakpoint=${ifDefined(args.breakpoint)}>
       ${steps}
     </kemet-tracker>
   `;
@@ -37,24 +42,11 @@ const Template = ({
 
 export const Standard: Story = {
   render: args => Template(args),
+};
+
+export const HideDotContent: Story = {
+  render: args => Template(args),
   args: {
-    breakpoint: '767px',
-    numOfSteps: 5,
-    currentStep: 3,
-    hideDotContent: false,
-  },
-  argTypes: {
-    breakpoint: {
-      control: { type: 'text' },
-    },
-    numOfSteps: {
-      control: { type: 'number' },
-    },
-    currentStep: {
-      control: { type: 'number', max: 5 },
-    },
-    hideDotContent: {
-      control: { type: 'boolean' },
-    },
+    hideDotContent: true,
   }
 };
