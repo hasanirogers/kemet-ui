efbd36441ffd49d3b404435266d5fdeb6f22aeb1
Hasani Rogers <hasani.rogers@gmail.com>
2024-01-28 15:16:23 -0500

    updated sandboxes to vite

--- sandboxes/htmlbox/web-dev-server.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  open: true,
-  nodeResolve: false,
-  appIndex: 'index.html',
-  rootDir: './',
-};
---
+++ sandboxes/vitebox/src/index.ts
@@ -0,0 +1,2 @@
+import 'kemet-ui';
+import 'kemet-ui/dist/styles/kemet.base.scss';
