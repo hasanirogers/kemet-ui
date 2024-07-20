af0abaaed9d955171b750d8eb286a23b70b9cb7a
Hasani Rogers <hasani.rogers@gmail.com>
2024-07-20 15:25:20 -0400

    added test for combo + draggable

---
+++ src/components/kemet-draggable/spec.ts
@@ -0,0 +1,36 @@
+import { html, render } from 'lit';
+import { expect } from '@wdio/globals';
+import { waitFor } from '@testing-library/dom';
+
+import './kemet-draggable';
+import drag from '../../utilities/misc/drag';
+
+describe('Draggable', () => {
+  const templates = {
+    default: html`
+      <kemet-draggable measure memory="memory">
+        <kemet-button>Drag Me!</kemet-button>
+      </kemet-draggable>
+    `,
+  };
+
+  // TODO: create test where drag is simulated
+  it('should render draggable', async () => {
+    render(templates.default, document.body);
+    const kemetDraggable = document.querySelector('kemet-draggable');
+    const initialX = kemetDraggable.left;
+    const initialY = kemetDraggable.top;
+
+    await waitFor(async () => {
+      const draggable = kemetDraggable.shadowRoot.querySelector('#draggable');
+
+      await drag(draggable, {
+        to: { x: 0, y: 0 },
+        delta: { x: 100, y: 100 },
+      });
+
+      expect(kemetDraggable.style.left).not.toBe(initialX);
+      expect(kemetDraggable.style.top).not.toBe(initialY);
+    });
+  });
+});
